3d931afd227aa4f3932e891ee2926b5350fc65b3
Saurav7985 <149267722+Saurav7985@users.noreply.github.com>
2024-08-31 10:45:38 +0000

    save

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14452
+let accountname = "sauravraikwar"
+var accountcity = "delhi"
+accountadd = "shuklana"
+
+// accountId = 25444
+
+console.log(accountId)
+
+accountname = "kumar"
+accountcity = "mumbai"
+accountadd = " malhara"
+
+console.table([accountId,accountcity,accountname]);
